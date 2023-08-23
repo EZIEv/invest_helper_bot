31ae501604b07bc8d0bc1d9bc43b1d5a96376f80
Danil Samuilov <52349137+EZIEv@users.noreply.github.com>
2023-08-23 20:38:52 +0300

    Update user_states.py

--- User_interaction/user_states.py
+++ User_interaction/user_states.py
@@ -1,7 +1,7 @@
 # Importing external modules
 from aiogram.dispatcher.filters.state import StatesGroup, State
 
-# Creating class of user's states
+# Creating a class of user's states
 class UserState(StatesGroup):
     # States for admins
     answer_id = State()
