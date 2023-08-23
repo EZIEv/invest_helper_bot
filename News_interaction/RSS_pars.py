042232f2ab8557e3f0efa41dcdf1919c5bc7e8a6
Danil Samuilov <52349137+EZIEv@users.noreply.github.com>
2023-08-23 20:36:54 +0300

    Update RSS_pars.py

--- News_interaction/RSS_pars.py
+++ News_interaction/RSS_pars.py
@@ -15,28 +15,28 @@ import aiohttp
 import config
 
 
-# Class for parsing news with RSS chanel
+# Class for parsing news with RSS channel
 class RSS_pars():
     def __init__(self):
         self.news_agregators = [(news_agregator[0].replace(' ', '_').lower(), news_agregator[1]) \
-                               for news_agregator in config.news_agregators] # List of tuples that contains: [0] - news agregator name, [1] - url on news agregator
+                               for news_agregator in config.news_agregators] # List of tuples that contains: [0] - news aggregator name, [1] - URL on news aggregator
         
-        self.old_news = {} # Creating dict-cache for collection old news
+        self.old_news = {} # Creating dict-cache for collection of old news
 
-        # Headers that provides requests to agregators' sites without blocking 
+        # Headers that provide requests to aggregators' sites without blocking 
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
 
-        # Importing old news from backup file if something get wrong with programm or create new deque if backup file doesn't exist
+        # Importing old news from the backup file if something gets wrong with the program or creating a new deque if the backup file doesn't exist
         for news_agregator in self.news_agregators:
             try:
                 with open(path.join('.', 'temp', f'{news_agregator[0]}_old_news.json'), 'r', encoding='utf-8') as old_news_file:
                     self.old_news[news_agregator[0]] = json.load(old_news_file)
-            # If file does not exist, so creating clear deque
+            # If the file does not exist, create a clear deque
             except FileNotFoundError:
                 self.old_news[news_agregator[0]] = deque([], 10)    
             except Exception as e:
                 logging.critical(f"Something went wrong with unloading data from temp files.\n\n{e}")
-            # If file exists, so creating deque and adding there cache news
+            # If the file exists, create a deque and add the cache news
             else:   
                 self.old_news[news_agregator[0]] = deque(self.old_news[news_agregator[0]], 10)
                 
@@ -90,14 +90,14 @@ class RSS_pars():
     '''---------------TITLE FUNCTION---------------'''
 
 
-    # Getting titles of news from rss http response
+    # Getting titles of news from RSS HTTP response
     async def get_title(self, news_response: str) -> list:
         titles = []
         for index, title in enumerate(re.finditer(r"<item>.*?<title>(?P<start>&lt;p&gt;|<!\[CDATA\[)?(?P<title>.*?)(?P<end>&lt;/p&gt;|\]\]>)?</title>", \
                                                   news_response, re.DOTALL)):
             titles.append(title.group('title'))
 
-            # Function only gets first 3 news and doen't read other for fast work
+            # Function only gets first 3 news and doesn't read others for fast work
             if index == 2:
                 break
         return titles
@@ -106,14 +106,14 @@ class RSS_pars():
     '''---------------LINK FUNCTION---------------'''
 
 
-    # Getting links of news from rss http response
+    # Getting links of news from RSS HTTP response
     async def get_link(self, news_response: str) -> list:
         links = []
         for index, link in enumerate(re.finditer(r"<item>.*?<link>(?P<start>&lt;p&gt;|<!\[CDATA\[)?(?P<link>http.+?)(?P<end>&lt;/p&gt;|\]\]>)?</link>", \
                                                   news_response, re.DOTALL)):
             links.append(link.group('link'))
 
-            # Function only gets first 3 news and doen't read other for fast work
+            # Function only gets first 3 news and doesn't read others for fast work
             if index == 2:
                 break
         return links
@@ -122,14 +122,14 @@ class RSS_pars():
     '''---------------DESCRIPTION FUNCTION---------------'''
 
 
-    # Getting description of news from rss http response
+    # Getting a description of news from RSS HTTP response
     async def get_description(self, news_response: str) -> list:
         descriptions = []
         for index, description in enumerate(re.finditer(r"<item>.*?<description>(?P<start>&lt;p&gt;|<!\[CDATA\[)?(?P<description>.*?)(?P<end>&lt;/p&gt;|\]\]>)?</description>", \
                                                   news_response, re.DOTALL)):
             descriptions.append(description.group('description'))
 
-            # Function only gets first 3 news and doen't read other for fast work
+            # Function only gets first 3 news and doesn't read others for fast work
             if index == 2:
                 break
         return descriptions
@@ -138,18 +138,18 @@ class RSS_pars():
     '''---------------PREVIEW IMAGE FUNCTION---------------'''
 
 
-    # Getting preview image with open graph from url link
+    # Getting a preview image with an open graph from a URL link
     async def get_preview_image(self, urls: tuple):
-        preview_images = [] # Initializating a clear list for possible images of news
+        preview_images = [] # Initializating a clear list of possible images of news
         async with aiohttp.ClientSession() as session:
             for url in urls:
-                # Getting response from news' link
+                # Getting response from new's link
                 async with session.get(url, headers=self.headers) as response:
                     if response.status == 200:
                         url_response = await response.text(encoding='utf-8')
                         image_url = re.search(r'<meta.*?property="og:image" content="(http.*?)".*?>', url_response, re.DOTALL)
 
-                        # If there is an image in opengraph, so we append image to list
+                        # If there is an image in opengraph, so we append the image to the list
                         if image_url:
                             preview_images.append(image_url.group(1))
                         else:
